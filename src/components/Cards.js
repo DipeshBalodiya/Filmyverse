ba6607309a970889b7e3da1fc40a23043ba8d5b2
DipeshBalodiya <133128548+DipeshBalodiya@users.noreply.github.com>
2023-08-27 12:39:28 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,6 @@ import { getDocs } from 'firebase/firestore'
 import { movieRef } from '../firebase/firebase'
 import { Link } from 'react-router-dom';
 
-const altfn = 'https://th.bing.com/th/id/R.4f23cc00214033cfd763a8758c308563?rik=g34m7WH166P%2fJA&riu=http%3a%2f%2fbra3.org%2fnot_found.png&ehk=mdYHaauK3wGDbbLzjetxPm8406z2bbrqp5bhz%2b0CKWA%3d&risl=&pid=ImgRaw&r=0"
 const Cards = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -30,7 +29,7 @@ const Cards = () => {
                     return (
                         <Link to={`/detail/${e.id}`}>
                         <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500'>
-                            <div className='image'><img className='h-60 md:h-72' alt={ altfn } src={e.image} /></div>
+                            <div className='image'><img className='h-60 md:h-72' alt="image link not found" src={e.image} /></div>
                             <h1>{e.title}</h1>
                             <h1 className='flex itme-center mr-2'><span className='text-gray-500 mt-1 mr-1'>Rating:</span>
                                 <ReactStars
